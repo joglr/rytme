63273ebdd6b2378b4bbd4a4e3a8851f903935f9f
Jonas Glerup Røssum <1959615+joglr@users.noreply.github.com>
2023-06-10 22:35:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-screen w-screen bg-red-500">
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
